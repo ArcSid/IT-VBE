7e2a4adbf188294b88dc8ce38cc586e036c55fe4
poppieFlower <raimundas.naujikas@gmail.com>
2023-08-25 16:08:39 +0300

    fixed 2019 U1

--- 2019/U1/main.cpp
+++ 2019/U1/main.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 using namespace std;
 
-void skaitymas(int &islaidos, int &aliejaus_kiekis, int turimi_indai[], int pardavimu_kainos[])
+void skaitymas(int &islaidos, int &aliejaus_kiekis, int turimi_indai[],
+               int pardavimu_kainos[])
 {
     ifstream data("U1.txt");
     data >> turimi_indai[0] >> turimi_indai[1] >> turimi_indai[2];
@@ -11,52 +12,50 @@ void skaitymas(int &islaidos, int &aliejaus_kiekis, int turimi_indai[], int pard
     data.close();
 }
 
-void pilnu_indu_skaiciavimas(int &aliejaus_kiekis, int turimi_indai[], int pilni_indai[])
+void pilnu_indu_skaiciavimas(int &aliejaus_kiekis, int turimi_indai[],
+                             int pilni_indai[])
 {
-    for (int i = 0; i < aliejaus_kiekis; i++)
-    {
-        if (turimi_indai[2] > 0 && aliejaus_kiekis / 5 > 0)
-        {
-            aliejaus_kiekis -= 5;
-            turimi_indai[2]--;
-            pilni_indai[2]++;
-            continue;
-        }
-        if (turimi_indai[1] > 0 && aliejaus_kiekis / 3 > 0)
-        {
-            aliejaus_kiekis -= 3;
-            turimi_indai[1]--;
-            pilni_indai[1]++;
-            continue;
-        }
-        if (turimi_indai[0] > 0 && aliejaus_kiekis > 0)
-        {
-            aliejaus_kiekis--;
-            turimi_indai[0]--;
-            pilni_indai[0]++;
-            continue;
-        }
-        break;
-    }
+    pilni_indai[2] = min(aliejaus_kiekis / 5, turimi_indai[2]);
+    aliejaus_kiekis -= pilni_indai[2] * 5;
+
+    pilni_indai[1] = min(aliejaus_kiekis / 3, turimi_indai[1]);
+    aliejaus_kiekis -= pilni_indai[1] * 3;
+
+    pilni_indai[0] = min(aliejaus_kiekis, turimi_indai[0]);
+    aliejaus_kiekis -= pilni_indai[0];
+
+    turimi_indai[2] -= pilni_indai[2];
+    turimi_indai[1] -= pilni_indai[1];
+    turimi_indai[0] -= pilni_indai[0];
 }
 
-int pelno_slaiciavimas(int islaidos, int papildomi_indai[], int pilni_indai[], int kainos[])
+int pelno_slaiciavimas(int islaidos, int papildomi_indai[], int pilni_indai[],
+                       int kainos[])
 {
-    return kainos[2] * (pilni_indai[2] + papildomi_indai[2]) + kainos[1] * (pilni_indai[1] + papildomi_indai[1]) + kainos[0] * (pilni_indai[0] + pilni_indai[0]) - islaidos;
+    return kainos[2] * (pilni_indai[2] + papildomi_indai[2]) +
+           kainos[1] * (pilni_indai[1] + papildomi_indai[1]) +
+           kainos[0] * (pilni_indai[0] + papildomi_indai[0]) - islaidos;
 }
 
-void kiek_reikia_indu(int papildomu_indu_kiekis[], int aliejaus_kiekis)
+void kiek_reikia_indu(int papildomu_indu_kiekis[], int likes_aliejus)
 {
-    papildomu_indu_kiekis[2] = aliejaus_kiekis / 5;
-    papildomu_indu_kiekis[1] = (aliejaus_kiekis - papildomu_indu_kiekis[2] * 5) / 3;
-    papildomu_indu_kiekis[0] = aliejaus_kiekis - (papildomu_indu_kiekis[2] * 5 + papildomu_indu_kiekis[1] * 3);
+    papildomu_indu_kiekis[2] = likes_aliejus / 5;
+    likes_aliejus -= 5 * papildomu_indu_kiekis[2];
+
+    papildomu_indu_kiekis[1] = likes_aliejus / 3;
+    likes_aliejus -= 3 * papildomu_indu_kiekis[1];
+
+    papildomu_indu_kiekis[0] = likes_aliejus / 1;
 }
 
-void rez(int pilni_indai[], int liko, int nepanaudoti[], int papildomi[], int pelnas)
+void rez(int pilni_indai[], int liko, int nepanaudoti[], int papildomi[],
+         int pelnas)
 {
     ofstream rez("U1rez.txt");
-    rez << pilni_indai[0] << " " << pilni_indai[1] << " " << pilni_indai[2] << " " << liko << endl;
-    rez << nepanaudoti[0] << " " << nepanaudoti[1] << " " << nepanaudoti[2] << endl;
+    rez << pilni_indai[0] << " " << pilni_indai[1] << " " << pilni_indai[2] << " "
+        << liko << endl;
+    rez << nepanaudoti[0] << " " << nepanaudoti[1] << " " << nepanaudoti[2]
+        << endl;
     rez << papildomi[0] << " " << papildomi[1] << " " << papildomi[2] << endl;
     rez << pelnas;
     rez.close();
@@ -80,7 +79,9 @@ int main()
 
     kiek_reikia_indu(papildomu_indu_kiekis, aliejaus_kiekis);
 
-    int pelnas = pelno_slaiciavimas(islaidos, papildomu_indu_kiekis, pilni_indai, pardavimu_kainos);
-    rez(pilni_indai, aliejaus_kiekis, nepanaudoti_indai, papildomu_indu_kiekis, pelnas);
+    int pelnas = pelno_slaiciavimas(islaidos, papildomu_indu_kiekis, pilni_indai,
+                                    pardavimu_kainos);
+    rez(pilni_indai, aliejaus_kiekis, turimi_indai, papildomu_indu_kiekis,
+        pelnas);
     return 0;
 }
