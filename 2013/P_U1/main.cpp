b561da767edafdc8377b9de05556a3488bd6d9f3
ArcSid <Raimundas.naujikas@gmail.com>
2024-02-03 21:54:44 +0200

    added 2013 U1

---
+++ 2013/P_U1/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+struct Uzsakovas{
+    string pavadinimas;
+    int x;
+    int y;
+    int atstumas(){
+        return abs(x)+abs(y)+abs(x)+abs(y);
+    }
+};
+
+void skaitymas(int& kilometrazas, vector<Uzsakovas>& uzsakovai){
+    ifstream data("U1.txt");
+    int n;
+    data >> n;
+    data >> kilometrazas;
+    for(int i = 0; i<n; i++){
+        data.ignore();
+        char ch[11];
+        data.read(ch, 10);
+        ch[10] = '\0';
+        Uzsakovas tempUzsakovas;
+        tempUzsakovas.pavadinimas = ch;
+        data >> tempUzsakovas.x;
+        data >> tempUzsakovas.y;
+        uzsakovai.push_back(tempUzsakovas);
+    }
+    data.close();
+}
+
+void skaiciavimai(int& likoUzsakovu, int& kiekNuvaziavo, string& paskutinis, int kilometrazas, vector<Uzsakovas> uzsakovai){
+    for(int i = 0; i<uzsakovai.size(); i++){
+        if(uzsakovai[i].atstumas() <= kilometrazas){
+            kiekNuvaziavo += uzsakovai[i].atstumas();
+            kilometrazas -= uzsakovai[i].atstumas();
+            likoUzsakovu --;
+            paskutinis = uzsakovai[i].pavadinimas;
+        }
+        else{
+            likoUzsakovu--;
+            kiekNuvaziavo += uzsakovai[i].atstumas();
+            paskutinis = uzsakovai[i].pavadinimas;
+            break; 
+        }
+    }
+}
+
+void rez(int likoUzsakovu, int kiekNuvaziavo, string paskutinis){
+    ofstream rez("U1rez.txt");
+    rez << likoUzsakovu << " " << kiekNuvaziavo << " " << paskutinis <<endl;
+    rez.close();
+}
+
+int main(){
+    int kilometrazas;
+    vector<Uzsakovas> uzsakovai;
+    skaitymas(kilometrazas, uzsakovai);
+    int likoUzsakovu = uzsakovai.size();
+    int kiekNuvaziavo = 0;
+    string paskutinis;
+    skaiciavimai(likoUzsakovu, kiekNuvaziavo, paskutinis, kilometrazas, uzsakovai);
+    rez(likoUzsakovu, kiekNuvaziavo, paskutinis);
+    return 0;
+}
