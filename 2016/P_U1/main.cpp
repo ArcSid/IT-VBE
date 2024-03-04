4e1fd85a943c6e6f6916b2db58fe700018e68d93
ArcSid <raimundas.naujikas@gmail.com>
2024-03-04 19:12:13 +0200

    formatted 2016 U1

--- 2016/P_U1/main.cpp
+++ 2016/P_U1/main.cpp
@@ -1,32 +1,37 @@
 #include <fstream>
 using namespace std;
 
-int vidurkis(){
+int vidurkis()
+{
     int ugiuSuma = 0;
     int kiekis;
     int ugis;
     ifstream data("U1.txt");
 
     data >> kiekis;
-    for(int i = 0; i<kiekis; i++){
+    for (int i = 0; i < kiekis; i++)
+    {
         data >> ugis;
         ugiuSuma += ugis;
     }
 
     data.close();
-    return ugiuSuma/kiekis;
+    return ugiuSuma / kiekis;
 }
 
-int didesniUzVidurki(int vidurkis){
+int didesniUzVidurki(int vidurkis)
+{
     int ugis;
     int visuKiekis;
     int aukstesniuKiekis = 0;
     ifstream data("U1.txt");
 
     data >> visuKiekis;
-    for(int i = 0; i<visuKiekis; i++){
+    for (int i = 0; i < visuKiekis; i++)
+    {
         data >> ugis;
-        if(ugis > vidurkis){
+        if (ugis > vidurkis)
+        {
             aukstesniuKiekis++;
         }
     }
@@ -35,13 +40,15 @@ int didesniUzVidurki(int vidurkis){
     return aukstesniuKiekis;
 }
 
-void rez(int vidurkis, int daugiauUzVidurkiKiekis){
+void rez(int vidurkis, int daugiauUzVidurkiKiekis)
+{
     ofstream rez("U1rez.txt");
     rez << vidurkis << " " << daugiauUzVidurkiKiekis << endl;
     rez.close();
 }
 
-int main(){
+int main()
+{
     int ugiuVidurkis = vidurkis();
     rez(ugiuVidurkis, didesniUzVidurki(ugiuVidurkis));
     return 0;
