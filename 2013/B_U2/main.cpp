7f95b00f45cb90f5e968223e523cd656ee14190d
ArcSid <raimundas.naujikas@gmail.com>
2024-05-13 18:44:49 +0300

    fixed 2013 Bandomasis U2

--- 2013/B_U2/main.cpp
+++ 2013/B_U2/main.cpp
@@ -7,7 +7,7 @@ void skaitytiIsFailo(int &batuKiekis, int lytys[], int kojosPuse[], int dydziai[
 {
     ifstream data("U2.txt");
     data >> batuKiekis;
-    
+
     for (int i = 0; i < batuKiekis; i++)
     {
         data >> lytys[i] >> kojosPuse[i] >> dydziai[i];
@@ -17,42 +17,59 @@ void skaitytiIsFailo(int &batuKiekis, int lytys[], int kojosPuse[], int dydziai[
 
 void rastiPoruKieki(int &vyriskuKiekis, int &moteriskuKiekis, int batuKiekis, int lytys[], int kojosPuse[], int dydziai[])
 {
-    int juodojoSarasoDydis = 0;
-    int juodasisSarasas[100];
+    int panaudotuBatuKiekis = 0;
+    int panaudotiBatai[100];
 
     for (int i = 0; i < batuKiekis; i++)
     {
         bool panaudotasElementas = false;
-        for (int j = 0; j < juodojoSarasoDydis; j++)
+        for (int j = 0; j < panaudotuBatuKiekis; j++)
         {
-            if (i == juodasisSarasas[j])
+            if (i == panaudotiBatai[j])
             {
                 panaudotasElementas = true;
                 break;
             }
         }
-        if (!panaudotasElementas)
-        {
-            for (int j = i; j < batuKiekis; j++)
-            {
-                if (lytys[i] == lytys[j] && kojosPuse[i] != kojosPuse[j] && dydziai[i] == dydziai[j])
-                {
-                    
-                    if (lytys[i] == 4)
-                    {
-                        moteriskuKiekis++;
-                    }
-                    else
-                    {
-                        vyriskuKiekis++;
-                    }
 
-                    juodasisSarasas[juodojoSarasoDydis] = i;
-                    juodojoSarasoDydis++;
-                    juodasisSarasas[juodojoSarasoDydis] = j;
-                    juodojoSarasoDydis++;
-                    break;
+        if (panaudotasElementas)
+        {
+            continue;
+        }
+
+        for (int j = i; j < batuKiekis; j++)
+        {
+            if (lytys[i] == lytys[j] && kojosPuse[i] != kojosPuse[j] && dydziai[i] == dydziai[j])
+            {
+                bool panaudotasElementas = false;
+                for (int k = 0; k < panaudotuBatuKiekis; k++)
+                {
+                    if (j == panaudotiBatai[k])
+                    {
+                        panaudotasElementas = true;
+                        break;
+                    }
                 }
+
+                if (panaudotasElementas)
+                {
+                    continue;
+                }
+
+                if (lytys[i] == 4)
+                {
+                    moteriskuKiekis++;
+                }
+                else
+                {
+                    vyriskuKiekis++;
+                }
+
+                panaudotiBatai[panaudotuBatuKiekis] = i;
+                panaudotuBatuKiekis++;
+                panaudotiBatai[panaudotuBatuKiekis] = j;
+                panaudotuBatuKiekis++;
+                break;
             }
         }
     }
@@ -74,7 +91,7 @@ int main()
 
     skaitytiIsFailo(batuKiekis, lytys, kojosPuse, dydziai);
 
-    int moteriskuKiekis, vyriskuKiekis;
+    int moteriskuKiekis = 0, vyriskuKiekis = 0;
     rastiPoruKieki(vyriskuKiekis, moteriskuKiekis, batuKiekis, lytys, kojosPuse, dydziai);
     rez(moteriskuKiekis, vyriskuKiekis);
 
