3accfc7f37be844e6170503d5443e4b97f95864d
ArcSid <raimundas.naujikas@gmail.com>
2024-05-19 11:57:33 +0300

    added 2014 bandomojo U1 ir U2

---
+++ 2014/B_U1/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+struct Uzsakovas
+{
+    int x;
+    int y;
+    int atstumasNuoPicerijosIrAtgal()
+    {
+        return (abs(x) + abs(y)) * 2;
+    }
+};
+
+void skaitytiIsFailo(int &kilometrazas, vector<Uzsakovas> &uzsakovai)
+{
+    Uzsakovas laikinasUzsakovas;
+    int uzsakovuKiekis;
+    ifstream data("U1.txt");
+
+    data >> uzsakovuKiekis >> kilometrazas;
+
+    for (int i = 0; i < uzsakovuKiekis; i++)
+    {
+        data >> laikinasUzsakovas.x >> laikinasUzsakovas.y;
+        uzsakovai.push_back(laikinasUzsakovas);
+    }
+    data.close();
+}
+
+void skaiciuotiKiekAptarnavoIrNuvaziavo(int &aptarnautuKiekis, int &nuvaziavo, int kilometrazas, vector<Uzsakovas> uzsakovai)
+{
+    for (int i = 0; i < uzsakovai.size(); i++)
+    {
+        if (uzsakovai[i].atstumasNuoPicerijosIrAtgal() + nuvaziavo > kilometrazas)
+        {
+            nuvaziavo += uzsakovai[i].atstumasNuoPicerijosIrAtgal();
+            aptarnautuKiekis++;
+            break;
+        }
+        nuvaziavo += uzsakovai[i].atstumasNuoPicerijosIrAtgal();
+        aptarnautuKiekis++;
+    }
+}
+
+void rez(int neaptarnautuKiekis, int nuvaziavo)
+{
+    ofstream rez("U1rez.txt");
+    rez << neaptarnautuKiekis << " " << nuvaziavo << endl;
+    rez.close();
+}
+
+int main()
+{
+    int kilometrazas;
+    vector<Uzsakovas> uzsakovai;
+    skaitytiIsFailo(kilometrazas, uzsakovai);
+
+    int aptarnautuKiekis = 0;
+    int nuvaziavo = 0;
+    skaiciuotiKiekAptarnavoIrNuvaziavo(aptarnautuKiekis, nuvaziavo, kilometrazas, uzsakovai);
+    rez(uzsakovai.size() - aptarnautuKiekis, nuvaziavo);
+}
